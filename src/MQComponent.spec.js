b1e4b2a6716785d56f1e2f04fc8c67a1dcfa92d6
Pavel Tereschenko <me@pavlik.pro>
2017-02-21 02:52:29 +0300

    Add default config tests

---
+++ src/MQComponent.spec.js
@@ -0,0 +1,116 @@
+// @flow
+
+/* eslint-env node, jest */
+/* eslint import/no-extraneous-dependencies: off */
+
+import React from 'react';
+import { mount } from 'enzyme';
+import matchMediaPolyfill from 'mq-polyfill';
+import type { ContextType } from 'mq-polyfill/lib/types.js.flow';
+
+import mqDecorate from './MQComponent';
+
+jest.useFakeTimers();
+
+
+const defaultConfig = {
+  isMobile: 'screen and (max-width: 767px)',
+  isTablet: 'screen and (min-width: 768px) and (max-width: 1023px)',
+  isDesktop: 'screen and (min-width: 1024px)',
+};
+
+type ExpectionType = ({
+  isMobile?: boolean,
+  isTablet?: boolean,
+  isDesktop?: boolean,
+  mq?: typeof defaultConfig,
+}) => {
+  isMobile: boolean,
+  isTablet: boolean,
+  isDesktop: boolean,
+  mq: typeof defaultConfig,
+};
+
+const getResExpection: ExpectionType = ({
+  isMobile = false,
+  isTablet = false,
+  isDesktop = false,
+  mq = defaultConfig,
+} = {}) => ({
+  isMobile,
+  isTablet,
+  isDesktop,
+  mq,
+});
+
+type TestComponentPropsType = {
+  mq?: { isMobile: string, isDesktop: string },
+  isMobile: boolean, isDesktop: boolean,
+};
+
+const TestComponent = (props: TestComponentPropsType) =>
+  <div>{JSON.stringify(props)}</div>;
+
+TestComponent.defaultProps = { mq: undefined };
+
+declare var window: ContextType;
+
+matchMediaPolyfill(window);
+
+const cteateMockedComponent = () => {
+  const Decorated = mqDecorate(TestComponent);
+
+  // $FlowIgnoreMock
+  Object.assign(
+    Decorated.prototype, {
+      setState: jest.fn(Decorated.prototype.setState),
+      componentDidMount: jest.fn(Decorated.prototype.componentDidMount),
+      render: jest.fn(Decorated.prototype.render),
+
+      subscribe: jest.fn(Decorated.prototype.subscribe),
+    },
+  );
+
+  return Decorated;
+};
+
+function resizeTo(width: number, height: number) {
+  Object.assign(window, {
+    innerWidth: width,
+    innerHeight: height,
+    // $FlowDefined
+  }).dispatchEvent(new window.Event('resize'));
+}
+
+const resizeWidth = (wigth: number, isRunTimers = true) => {
+  resizeTo(wigth, window.innerHeight);
+  if (isRunTimers) jest.runAllTimers();
+};
+
+describe('mqDecorate()', () => {
+  it('Have the default config with "isMobile", "isTablet" and "isDesktop" breakpoints', () => {
+    resizeWidth(1024);
+
+    const Decorated = cteateMockedComponent();
+    const component = mount(<Decorated />);
+
+    expect(component.find(TestComponent).props())
+      .toEqual(getResExpection({ isDesktop: true }));
+
+    resizeWidth(1023);
+    expect(component.find(TestComponent).props())
+      .toEqual(getResExpection({ isTablet: true }));
+
+    resizeWidth(768);
+    expect(component.find(TestComponent).props())
+      .toEqual(getResExpection({ isTablet: true }));
+
+    resizeWidth(767);
+    expect(component.find(TestComponent).props())
+      .toEqual(getResExpection({ isMobile: true }));
+
+    resizeWidth(480);
+    expect(component.find(TestComponent).props())
+      .toEqual(getResExpection({ isMobile: true }));
+  });
+});
